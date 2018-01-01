71f2f2c3f0788112d9403b6662877c6a1144e49a
DirtK <oklow@yandex.ru>
2018-01-01 05:27:09 +0300

    Created some car objects in OOP style

---
+++ src/ru/moneybranch/webapp/model/Resume.java
@@ -0,0 +1,17 @@
+package ru.moneybranch.webapp.model;
+
+/**
+ * stanley
+ * 31.12.17.
+ */
+public class Resume {
+
+    private String firstName;
+    private String lastName;
+    private String patronymic;
+    private String fullName;
+
+
+
+
+}
---
+++ src/ru/moneybranch/webapp/model/auxiliary/ConsoleColors.java
@@ -0,0 +1,34 @@
+package ru.moneybranch.webapp.model.auxiliary;
+
+public enum ConsoleColors {
+    ANSI_RESET("\u001B[0m"),
+    ANSI_BLACK( "\u001B[30m"),
+    ANSI_RED("\u001B[31m"),
+    ANSI_GREEN("\u001B[32m"),
+    ANSI_YELLOW ("\u001B[33m"),
+    ANSI_BLUE ("\u001B[34m"),
+    ANSI_PURPLE("\u001B[35m"),
+    ANSI_CYAN ("\u001B[36m"),
+    ANSI_WHITE ("\u001B[37m");
+
+    /**
+     *  stanley
+     01.12.17.
+     */
+    private String color;
+
+    ConsoleColors(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return getColor();
+    }
+}
+
+
